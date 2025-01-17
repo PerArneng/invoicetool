f78689a5d98eb38e610dfb1b33d249360ce24d3b
Per Arneng <per.arneng@scalebit.com>
2025-01-17 16:17:40 +0100

    improved cli parsing

--- invoicetool/src/cli/args.rs
+++ invoicetool/src/cli/args.rs
@@ -3,14 +3,13 @@ use std::path::PathBuf;
 
 use clap::{arg, command, value_parser, Arg, ArgAction, Command};
 
-pub struct Args<'a> {
-    pub invoice_file_path: &'a PathBuf,
+#[derive(Debug)]
+pub struct Args {
+    pub invoice_file_path: PathBuf,
+    pub auto_confirm: bool,
 }
 
-pub fn parse() {
-    
-    println!("hi");
-    
+pub fn parse() -> Args {
     let matches = command!() // requires `cargo` feature
         .arg(arg!([name] "Optional name to operate on"))
         .arg(
@@ -37,20 +36,18 @@ pub fn parse() {
         )
         .get_matches();
 
+    let mut temp_invoice_file_path: Option<PathBuf> = None;
+    let mut temp_auto_confirm: bool = false;
+
     if let Some(matches) = matches.subcommand_matches("rename") {
         if let Some(invoice_file_path) = matches.get_one::<PathBuf>("file") {
-            println!("invoice file: {}", invoice_file_path.display());
+            temp_invoice_file_path = Some(invoice_file_path.clone());
         }
-        if let Some(rename_auto_confirm) = matches.get_one::<bool>("yes") {
-            println!("invoice rename auto confirm: {}", rename_auto_confirm);
-        }
+        temp_auto_confirm = matches.get_flag("yes");
     }
 
-    
-
-
-        //Args {
-        //    invoice_file_path: matches.get_one::<PathBuf>("config").unwrap(),
-       // }
-    
+    Args {
+        invoice_file_path: temp_invoice_file_path.expect("unexpected missing invoice file path"),
+        auto_confirm: temp_auto_confirm,
+    }
 }
--- invoicetool/src/main.rs
+++ invoicetool/src/main.rs
@@ -3,8 +3,17 @@ mod cli;
 use invoicelib::add;
 
 fn main() {
-    cli::args::parse();
+    let args = cli::args::parse();
+    
+    if !args.invoice_file_path.exists() {
+        //FIXME: log using proper logging library
+        eprintln!("Error: Invoice file '{}' does not exist", args.invoice_file_path.display());
+        std::process::exit(1);
+    }
+
+    println!("args: {:?}", args); //FIXME: log using proper logging library
+    
     let a = add(1, 2);
-    println!("Hello, world! {}", a);
+    println!("testnum {}", a);
     
 }
